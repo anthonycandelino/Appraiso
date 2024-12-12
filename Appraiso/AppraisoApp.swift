b7fbe3a74e009ce309117c26a871ba2bc19f772f
Anthony Candelino <anthcandelino@gmail.com>
2024-12-11 21:12:51 -0500

    Initial Commit

---
+++ Appraiso/AppraisoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppraisoApp.swift
+//  Appraiso
+//
+//  Created by Anthony Candelino on 2024-12-11.
+//
+
+import SwiftUI
+
+@main
+struct AppraisoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
